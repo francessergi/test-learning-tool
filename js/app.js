a66a79ba1538fd43b54e2699805fa3888d14bb49
francessergi <60177653+francessergi@users.noreply.github.com>
2021-02-06 18:17:20 +0100

    repair

--- js/app.js
+++ js/app.js
@@ -1,29 +1,156 @@
-var bdd = [
-    ["Buenos días / Buenas noches", "Annyong"],
-    ["Entiendo / No entiendo", "Ihae / nan ihaega an dwaeyo"],
+var target = [];
+
+var basicas = [
+    ["Buenos días", "Annyong"],
+    ["Lo entiendo", "Ihae haess-eoyo"],
     ["Hasta luego", "Annyonghi kyeseyo"],
-    ["Bienvenido", "Hwan-yeong"],
-    ["Gracias (muchas)", "Gamsahamnida"],
+    ["Bienvenido", "Hwan-yeong-amnida"],
+    ["Gracias", "Gamsahamnida"],
     ["Perdona / Disculpa", "Sillyehamnida"],
-    ["No gracias", "Gwaenchanhseubnida"],
-    ["Si / No", "Ne / Aniyo"]
+    ["No gracias", "Gwaenchan-ayo"],
+    ["Si", "Ne"],
+    ["No", "Aniyo"]
 ];
 
-bbdRnd = [].concat(bdd);
-shuffle(bbdRnd);
+var negocios = [
+    ["Cuánto es?", "Eolmana?"],
+    ["Es barato", "Geugeos-eun maeu jeolyeom"],
+    ["Es demasiado caro !", "Geugeos-eun neomu bissa"],
+    ["Me encanta", "Naneun salang"],
+    ["Lo detesto", "Silh-eo"],
+    ["Dinero", "Don"]
+];
 
+var transportes = [
+    ["Avión", "Bihaengki"],
+    ["Barco", "Boteu"],
+    ["Tren", "Kicha"],
+    ["Taxi", "Taeksi"],
+    ["Bus / Autobús", "Beoseu"],
+    ["Me gustaría alquilar", "Naneun imdaehago sip-eoyo..."],
+    ["Moto", "Otobai"],
+    ["Coche", "Seungyongcha"],
+    ["Bici / Bicicleta", "Jajeongeo"]
+];
+
+var direcciones = [
+    ["Banco", "Eunhaeng"],
+    ["Estación", "Kicha yeok"],
+    ["Centro Cuidad", "Dosim"],
+    ["Hotel", "Hoteil"],
+    ["Hospital", "Byeongwon"],
+    ["Derecha", "Orun-jog"],
+    ["Izquierda", "Wen-jog"],
+    ["Norte/Sur/Este/Oeste", "Buk/nam/dong/seo"]
+];
+
+var cifras = [
+    ["uno", "ana"],
+    ["dos", "dul"],
+    ["tres", "set"],
+    ["cuatro", "net"],
+    ["cinco", "tasot"],
+    ["seis", "yosot"],
+    ["siete", "iglop"],
+    ["ocho", "yodolp"],
+    ["ahop", "net"],
+    ["cuatro", "yol"]
+];
+
+var horario = [
+    ["Qué hora es?", "Geugeos-eun myeochsi ibnikka ?"],
+    ["Cuándo?", "Eonje ?"],
+    ["Ayer", "Eoje"],
+    ["Hoy (mañana/tarde/noche)", "Oneul (achim/daenaj/jeonyeog)"],
+    ["Mañana", "Nae-il"],
+    ["Lunes, martes, miércoles, jueves, viernes, sábado, domingo", "Wol-yoil, hwayoil, suyoil, mog-yoil, geum-yoil, toyoil, il-yoil"]
+];
+
+var comida = [
+    ["Tengo hambre/ tengo sed", "Baegopa / naneun mog maleun ibnida"],
+    ["Estaba delicioso! / Estaba riquísimo", "Masisseosseumnida"],
+    ["Qué me recomiendas?", "Mwol chucheon haeyo"],
+    ["Soy vegetariano", "Jeon chaeshikjuyija"],
+    ["Está demasiado caliente", "Neomu tteugeobda"],
+    ["Soy alérgico(a)", "Naneun alleleugi ibnida"],
+    ["Marisco", "Haesanmul"],
+    ["Cacahuete", "Ttangkong"],
+    ["Gluten", "Geullutin"],
+    ["Agua", "Mul"],
+    ["Té/café", "Hongcha / Keopi"],
+    ["Cerveza/vino", "Maekju / podoju"]
+];
+
+var salud = [
+    ["Dónde esta el hospital?", "Eodi byeong-won ibnida"],
+    ["Me duele aquí", "Yeogi sangcheo"],
+    ["Dónde estan los baños?", "Hwajangsil eun eodie issseubnikka?"],
+    ["Socorro ! / Ayuda !", "Dowajuseyo!"],
+    ["Policia", "Gyeongchal"],
+    ["Peligro", "Wiheom"]
+];
+
+function escullTest(param) {
+    var x = param.value;
+    if (questions.childElementCount > 1) {
+        eraseAll();
+    }
+    switch (x) {
+        case "Expresiones basicas":
+            target = basicas;
+            break;
+        case "Negocios":
+            target = negocios;
+            break;
+        case "Transportes":
+            target = transportes;
+            break;
+        case "Direcciones":
+            target = direcciones;
+            break;
+        case "Cifras":
+            target = cifras;
+            break;
+        case "Horario":
+            target = horario;
+            break;
+        case "Comida":
+            target = comida;
+            break;
+        case "Salud":
+            target = salud;
+            break;
+        default:
+            break;
+    }
+    targetRandom = [].concat(target);
+    shuffle(targetRandom);
+    carregaTest();
+}
+
+
+
+function select() {
+    var coleccion = ["Expresiones basicas", "Negocios", "Transportes", "Direcciones", "Cifras", "Horario", "Comida", "Salud"];
+    var tests = document.getElementById('tests');
+    for (let index = 0; index < coleccion.length; index++) {
+        var opcio = document.createElement('option');
+        opcio.value = coleccion[index];
+        opcio.textContent = coleccion[index];
+        tests.appendChild(opcio);
+    }
+}
 
 
 //questions - lContainer - rContainer
 function carregaTest() {
-    bdd.forEach(element => {
+    target.forEach(element => {
         var question = document.createElement('div');
         question.textContent = element[0];
         question.classList.add('question');
         questions.appendChild(question);
-
     });
-    bbdRnd.forEach(element => {
+    targetRandom.forEach(element => {
         var answer = document.createElement('div');
         answer.textContent = element[1];
         answer.classList.add('answer');
@@ -43,25 +170,32 @@ function canviaLloc(param) {
     }
 }
 
-function reset() {
+function eraseAll() {
     var eQuestion = document.getElementsByClassName('question');
     var eAnswer = document.getElementsByClassName('answer');
     while (eAnswer.length > 0) {
         eQuestion[0].remove();
         eAnswer[0].remove();
     }
+    //carregaTest();
+}
+
+function resetAll() {
+    eraseAll()
     carregaTest();
 }
 
+
+
 function evalue() {
     var eAnswer = document.getElementsByClassName('answer');
-    if (eAnswer.length != bdd.length) {
+    if (lContainer.childElementCount - 1 != target.length) {
         console.log("Falten preguntes per respondre");
         return false;
     }
     for (let index = 0; index < eAnswer.length; index++) {
         var answer = eAnswer[index].textContent;
-        if (answer == bdd[index][1]) {
+        if (answer == target[index][1]) {
             eAnswer[index].style.background = "#1AD741";
         } else {
             eAnswer[index].style.background = "#E30300";
---
+++ tests.md
@@ -0,0 +1,83 @@
+/* var coleccion = ["Expersiones basicas","Negocios","Transportes","Direcciones","Cifras","Horario","Comida"];
+
+var expersionesBasicas = [
+    ["Buenos días","Annyong"],
+    ["Lo entiendo","Ihae haess-eoyo"],
+    ["Hasta luego","Annyonghi kyeseyo"],
+    ["Bienvenido","Hwan-yeong-amnida"],
+    ["Gracias","Gamsahamnida"],
+    ["Perdona / Disculpa","Sillyehamnida"],
+    ["No gracias","Gwaenchan-ayo"],
+    ["Si","Ne"],
+    ["No","Aniyo"]
+];
+
+var negocios = [
+    ["Cuánto es?","Eolmana ?"],
+    ["Es barato","Geugeos-eun maeu jeolyeom"],
+    ["Es demasiado caro !","Geugeos-eun neomu bissa"],
+    ["Me encanta / Lo detesto","Naneun salang / silh-eo"],
+    ["Dinero","Don"]
+];
+
+var transportes = [
+    ["Avión","Bihaengki"],
+    ["Barco","Boteu"],
+    ["Tren","Kicha"],
+    ["Taxi","Taeksi"],
+    ["Bus / Autobús","Beoseu"],
+    ["Me gustaría alquilar","Naneun imdaehago sip-eoyo..."],
+    ["Moto","Otobai"],
+    ["Coche","Seungyongcha"],
+    ["Bici / Bicicleta","Jajeongeo"]
+];
+
+var direcciones = [
+    ["Banco","Eunhaeng"],
+    ["Estación","Kicha yeok"],
+    ["Centro Cuidad","Dosim"],
+    ["Hotel","Hoteil"],
+    ["Hospital","Byeongwon"],
+    ["Izquierda/Derecha","Wen-jog / Orun-jog"],
+    ["Norte/Sur/Este/Oeste","Buk/nam/dong/seo"]
+];
+
+var cifras = [
+    ["uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve y diez","ana, tul, set, net, tasot, yosot, iglop, yodolp, ahop, yol"],
+    ["veinte, treinta, cuarenta, cincuenta, sesenta","sumul, sorun, mahun, shween, yesun"],
+    ["setenta, ochenta, noventa","irun, yodun, ahon,"],
+    ["cien","baeg"]
+];
+
+var horario = [
+    ["Qué hora es?","Geugeos-eun myeochsi ibnikka ?"],
+    ["Cuándo?","Eonje ?"],
+    ["Ayer","Eoje"],
+    ["Hoy (mañana/tarde/noche)","Oneul (achim/daenaj/jeonyeog)"],
+    ["Mañana","Nae-il"],
+    ["Lunes, martes, miércoles, jueves, viernes, sábado, domingo","Wol-yoil, hwayoil, suyoil, mog-yoil, geum-yoil, toyoil, il-yoil"]
+];
+
+var comida = [
+    ["Tengo hambre/ tengo sed","Baegopa / naneun mog maleun ibnida"],
+    ["Estaba delicioso! / Estaba riquísimo","Masisseosseumnida"],
+    ["Qué me recomiendas?","Mwol chucheon haeyo"],
+    ["Soy vegetariano","Jeon chaeshikjuyija"],
+    ["Está demasiado caliente","Neomu tteugeobda"],
+    ["Soy alérgico(a)","Naneun alleleugi ibnida"],
+    ["Marisco","Haesanmul"],
+    ["Cacahuete","Ttangkong"],
+    ["Gluten","Geullutin"],
+    ["Agua","Mul"],
+    ["Té/café","Hongcha / Keopi"],
+    ["Cerveza/vino","Maekju / podoju"]
+];
+
+var salud = [
+    ["Dónde esta el hospital?","Eodi byeong-won ibnida"],
+    ["Me duele aquí","Yeogi sangcheo"],
+    ["Dónde estan los baños?","Hwajangsil eun eodie issseubnikka?"],
+    ["Socorro ! / Ayuda !","Dowajuseyo!"],
+    ["Policia","Gyeongchal"],
+    ["Peligro","Wiheom"]
+]; */
